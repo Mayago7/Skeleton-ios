1621ff24a3776e1f841bbfa70eabc66016eda13c
Bernardo Cervantes Mayagoitia <36172359+Mayago7@users.noreply.github.com>
2026-04-06 11:46:05 -0600

    Add files via upload

---
+++ Skeleton-ios/Skeleton_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Skeleton_iosApp.swift
+//  Skeleton-ios
+//
+//  Created by Bernardo Cervantes Mayagoitia on 06/04/26.
+//
+
+import SwiftUI
+
+@main
+struct Skeleton_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Skeleton-iosTests/Skeleton_iosTests.swift
@@ -0,0 +1,19 @@
+//
+//  Skeleton_iosTests.swift
+//  Skeleton-iosTests
+//
+//  Created by Bernardo Cervantes Mayagoitia on 06/04/26.
+//
+
+import Testing
+@testable import Skeleton_ios
+
+struct Skeleton_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
